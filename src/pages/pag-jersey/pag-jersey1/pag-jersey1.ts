e123768f9046681381fca36b531d4de46ba6e468
IrisAlves <irisalvesfranco@gmail.com>
2019-02-06 23:53:04 +0100

    casa

--- src/pages/pag-jersey/pag-jersey1/pag-jersey1.ts
+++ src/pages/pag-jersey/pag-jersey1/pag-jersey1.ts
@@ -1,6 +1,7 @@
+import { ListaFavProvider } from './../../../providers/lista-fav/lista-fav';
+import { Fav } from './../../../models/Fav-item/Fav-item.inteface';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 @IonicPage()
 @Component({
   selector: 'page-pag-jer1',
@@ -9,7 +10,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class PagJersey1Page {
 
   images=['1.jpg','2.jpg','3.jpg'];
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public LFavPro: ListaFavProvider,public alertCtrl:AlertController) {
   }
 
   ionViewDidLoad() {
